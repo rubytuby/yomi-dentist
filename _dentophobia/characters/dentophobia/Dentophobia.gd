d0dacde253506ebe06a677667ea85fec0c1b5f52
Liz <adammvehige@gmail.com>
2024-10-17 09:47:34 -0500

    setup for playerinfo scene

--- _dentophobia/characters/dentophobia/Dentophobia.gd
+++ _dentophobia/characters/dentophobia/Dentophobia.gd
@@ -2,3 +2,13 @@ extends Fighter
 
 class_name Dentophobia
 
+const TEETH_COUNT = 10
+const TEETH_PER_USE = 1
+
+var currentTeeth = 0
+
+func _on_hit_something(obj, hitbox):
+	._on_hit_something(obj, hitbox)
+	if (currentTeeth < TEETH_COUNT):
+		currentTeeth + 1
+		
