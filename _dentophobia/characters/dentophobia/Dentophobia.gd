d9f97e8ea671cbcd3b8db703510107149111365f
Liz <adammvehige@gmail.com>
2024-10-17 20:18:56 -0500

    trying to make sure you dont gain teeth on the same turn you spend teeth

--- _dentophobia/characters/dentophobia/Dentophobia.gd
+++ _dentophobia/characters/dentophobia/Dentophobia.gd
@@ -6,9 +6,10 @@ const TEETH_COUNT = 10
 const TEETH_PER_USE = 1
 
 var currentTeeth = 0
+var usedTeethThisTurn = false
 
 func _on_hit_something(obj, hitbox):
 	._on_hit_something(obj, hitbox)
-	if (currentTeeth < TEETH_COUNT):
+	if (currentTeeth < TEETH_COUNT && usedTeethThisTurn == false):
 		currentTeeth + 1
 		
